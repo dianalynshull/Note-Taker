4d14bb16a83ad9805f17ffcf6550b2c1eb9085d0
dianalynshull <dianalynshull@gmail.com>
2020-12-24 01:01:29 -0700

    un-commented out the apiroutes in the db. Added get and post routes for /api/notes and verified that they are working

--- develop/routes/apiRoutes.js
+++ develop/routes/apiRoutes.js
@@ -1,5 +1,19 @@
-// api get route for getting already created notes
+const db = require('../db/db.json');
 
-// api post route to create new notes, store them in the database, and then display them on the page
-
-// app delete route to delete notes from the database
+module.exports = app => {
+    // api get route for getting already created notes
+    app.get('/api/notes', (req, res) => {
+        res.json(db);
+    });
+    
+    // api post route to create new notes, store them in the database, and then display them on the page
+    app.post('/api/notes', async (req, res) => {
+        try {
+            db.push(req.body);
+            res.json(true);
+        } catch (err) {
+            console.log(err);
+        };
+    });
+    // app delete route to delete notes from the database
+};
