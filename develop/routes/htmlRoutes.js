ba23b00b258629602d266ad25ff0c7c88807f1ca
dianalynshull <dianalynshull@gmail.com>
2020-12-23 23:28:34 -0700

    commented out the api routes link in the server file while I work on the html routes. Added start script to package.json. Required in path to the html routes and added the first route to index.

--- develop/routes/htmlRoutes.js
+++ develop/routes/htmlRoutes.js
@@ -1,3 +1,13 @@
-// html route for the index page
+const path = require('path');
 
-// html route for the notes page
+module.exports = app => {
+
+    // html route for the index page
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/index.html'));
+    });
+    // html route for the notes page
+
+    // html route to send any undefined routes to index
+
+};
--- develop/server.js
+++ develop/server.js
@@ -2,13 +2,11 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-require('./routes/apiRoutes')(app);
+// require('./routes/apiRoutes')(app);
 require('./routes/htmlRoutes')(app);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-
 
 app.listen(PORT, () => {
     console.log('App is listening on PORT: ' + PORT);
