54f93be7bf39b715c45664b7c71e733fc9b17040
dianalynshull <dianalynshull@gmail.com>
2020-12-24 00:54:07 -0700

    moved my package.json, package-lock.json, .gitignore, and node modules into the develop folder. Also was having rendering problems and realized that I was importing app in my routes before middleware. Moved the routes below. Still was having problems. Realized I needed to define my static folder. Added that to my server file.

--- develop/server.js
+++ develop/server.js
@@ -2,11 +2,12 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// require('./routes/apiRoutes')(app);
-require('./routes/htmlRoutes')(app);
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
+
+// require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
 
 app.listen(PORT, () => {
     console.log('App is listening on PORT: ' + PORT);
