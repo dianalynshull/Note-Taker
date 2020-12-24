881ed2875fb8b041cf2c45ddf2990adc99359bdd
dianalynshull <dianalynshull@gmail.com>
2020-12-23 23:32:14 -0700

    added notes html route

--- develop/routes/htmlRoutes.js
+++ develop/routes/htmlRoutes.js
@@ -7,7 +7,9 @@ module.exports = app => {
         res.sendFile(path.join(__dirname, '../public/index.html'));
     });
     // html route for the notes page
-
+    app.get('/notes', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/notes.html'));
+    });
     // html route to send any undefined routes to index
 
 };
